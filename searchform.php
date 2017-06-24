93ea4b2418550bcd2c293c99cad9524b80e5055b
Dan <dlaubacher3@gmail.com>
2017-06-24 16:10:14 -0400

    update search menu for fade in effect

--- searchform.php
+++ searchform.php
@@ -1,26 +1,14 @@
+<div id="searchBar" data-toggler=".expanded" data-animate="fade-in fade-out" class="small-12 medium-6 columns search" expanded-mobile>
 
-<div id="searchBar" data-toggler=".expanded" class="row search expanded-mobile">
-  <div class="small-12 columns">
-    <div class="row align-center collapse">
-      <!-- Search field -->
+          <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+            <label>
+                <input type="search" class="search-field"
+                placeholder="<?php echo esc_attr_x( 'Search…', 'placeholder', 'timber-news' ) ?>"
+                value="<?php echo get_search_query() ?>" name="s"
+                title="<?php echo esc_attr_x( 'Search for:', 'label', 'timber-news' ) ?>" />
+            </label>
+            <input type="submit"
+            value="<?php echo esc_attr_x( 'Search', 'submit button', 'timber-news' ) ?>" />
+          </form>
 
-      <div class="small-12 large-6 search columns">
-
-        <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-
-          <label>
-              <input type="search" class="search-field"
-              placeholder="<?php echo esc_attr_x( 'Search…', 'placeholder', 'timber-news' ) ?>"
-              value="<?php echo get_search_query() ?>" name="s"
-              title="<?php echo esc_attr_x( 'Search for:', 'label', 'timber-news' ) ?>" />
-          </label>
-
-          <input type="submit"
-          value="<?php echo esc_attr_x( 'Search', 'submit button', 'timber-news' ) ?>" />
-
-        </form>
-
-      </div>
-    </div>
   </div>
-</div>
